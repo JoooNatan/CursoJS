b51548f753ac221065eeacd67068ad5d3ef83fde
João Natan <joaonatan67548@gmail.com>
2022-09-30 23:31:51 -0300

    desafio 03 feito

---
+++ desafios/d003/script.js
@@ -0,0 +1,5 @@
+function AeS() {
+    var num = 0
+    num = window.prompt('Digite um numero inteiro qualquer')
+    window.alert(`Antes de ${num}, temos ${num - 1}\nDepois de ${num}, temos ${Number(num) + 1}`)
+}
