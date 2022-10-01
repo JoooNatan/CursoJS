666b7371e725bf40554f83a68544371efe5f48cc
João Natan <joaonatan67548@gmail.com>
2022-09-30 23:08:59 -0300

    desafio 01 feito

---
+++ desafios/d001/script.js
@@ -0,0 +1,9 @@
+function Mensagem(n) {
+    if (n == 1) {
+        window.alert('Voce aperou o botão 1')
+    } else if (n == 2) {
+        window.alert('Voce aperou o botão 2')
+    } else {
+        window.alert('Voce aperou o botão 3')
+    }
+}
